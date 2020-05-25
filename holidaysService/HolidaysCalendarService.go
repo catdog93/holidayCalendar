bb42d272841e7f978889c0e3fbbe85d1327b6e0f
catdog93 <93catdog@gmail.com>
2020-05-25 19:54:36 +0300

    First beta release without testing

--- holidaysService/HolidaysCalendarService.go
+++ holidaysService/HolidaysCalendarService.go
@@ -14,10 +14,7 @@ const AverageNumberOfPublicHolidaysPerYear = 12
 
 const PublicHolidaysURL = "https://date.nager.at/api/v2/publicholidays/2020/UA"
 
-//const CountriesCodeURL = "https://restcountries.eu/rest/v2/alpha?codes=col;no;ee"
-const CountriesCodeURL = "https://api.printful.com/countries"
-
-const ErrStringGetCountriesCodes = "error occurred in holidaysService.GetPublicHolidays(): "
+const ErrStringGetCountriesCodes = "error occurred in holidaysService.getPublicHolidays(): "
 
 type CountryCode string
 type Obj []interface{}
@@ -25,23 +22,9 @@ type Obj []interface{}
 const CustomTimeFormat = "2006-01-02"
 
 type HolidaysCalendar struct {
-	//TempCache          *Obj // caches
-	PublicHolidaysResponse *[]GetPublicHolidaysResponse `json:"countriesCodes"`
-	HolidaysCash           map[float64]time.Time        `json:"holidaysCash"`
-	/*CountriesCodeCache *Obj
-	HolidaysCache      *Obj*/
-}
-
-type CountriesCodes struct {
-	Code   int                   `json:"code"`
-	Result []CountriesCodeResult `json:"result"`
-
-	/*"code": 200,
-	  "result": [{
-	      "code": "AF",
-	      "name": "Afghanistan",
-	      "states": null
-	  },*/
+	PublicHolidaysResponse         *[]GetPublicHolidaysResponse `json:"countriesCodes"`
+	HolidaysDifferenceSinceNowList *[]float64                   `json:"holidays"`
+	*PublicHolidays
 }
 
 /*"date": "2020-01-07",
@@ -66,63 +49,64 @@ type GetPublicHolidaysResponse struct {
 	Type             string      `json:"type"`
 }
 
-type CountriesCodeResult struct {
-	Code   string      `json:"code"`
-	Name   string      `json:"name"`
-	States interface{} `json:"states"`
+type PublicHolidays struct {
+	HolidaysDatesList []time.Time `json:"holidaysDates"`
+	HolidaysNamesList []string    `json:"holidaysNames"`
 }
 
-/*
-func (service *CalendarService) ServeHTTP(response http.ResponseWriter, request *http.Request) {
-	//response.Header().Set("Content-type", "application/json")
-
-}*/
-
-type CalendarService interface {
-	GetHolidaysForThisYear(year int, code CountryCode) (err error)
-	GetCountriesCodes() (err error)
-}
-
-// Method support year since 2000 to current year + 40 (2060). Country code format: "UA", "AT", "BE", "GB" (2 chars)
-/*func (calendar *HolidaysCalendar) GetHolidaysForThisYear(year int, code CountryCode, serviceURL string) error {
-	errorString := "error holidaysService.GetHolidaysForThisYear(): "
-	if url, err := url.Parse(serviceURL); err != nil {
-		errorString += err.Error()
+func (calendar *HolidaysCalendar) initCaches() error {
+	if PublicHolidaysResponse, err := calendar.getPublicHolidays(PublicHolidaysURL); err != nil {
+		return err
 	} else {
-		if resp, err := http.Get(url.String()); err != nil { // when there is an error, the response will be nil and an error will be returned.
-			errorString += err.Error()
-		}
-		if resp != nil { // But, when you get a redirection error, response will not be nil but there’ll be an error.
-			defer resp.Body.Close()/////
-			body, err := ioutil.ReadAll(resp.Body)
-				calendar.TempCache = &Obj{}
-				if err = json.Unmarshal(body, calendar.TempCache); err != nil {
+		calendar.PublicHolidaysResponse = PublicHolidaysResponse
+		todayDate := time.Now()
+		todayDate = time.Date(todayDate.Year(), 6, 20, 0, 0, 0, 0, time.UTC)
+		calendar.HolidaysDifferenceSinceNowList = &[]float64{}
+		calendar.PublicHolidays = &PublicHolidays{}
 
+		*calendar.HolidaysDifferenceSinceNowList = make([]float64, 0, AverageNumberOfPublicHolidaysPerYear)
+		calendar.HolidaysDatesList = make([]time.Time, 0, AverageNumberOfPublicHolidaysPerYear)
+		calendar.HolidaysNamesList = make([]string, 0, AverageNumberOfPublicHolidaysPerYear)
+
+		for _, value := range *calendar.PublicHolidaysResponse {
+			if holidayDate, err := time.Parse(CustomTimeFormat, value.DateStringFormat); err != nil {
+				return err
+			} else {
+				if duration := todayDate.Sub(holidayDate).Hours(); duration <= 0 {
+					*calendar.HolidaysDifferenceSinceNowList = append(*calendar.HolidaysDifferenceSinceNowList, math.Abs(duration))
+					calendar.HolidaysDatesList = append(calendar.HolidaysDatesList, holidayDate)
+					calendar.HolidaysNamesList = append(calendar.HolidaysNamesList, value.Name)
 				}
-} else {
-
-
-		}
-	}
-
-	if year >= 2000 && year <= time.Now().Year()+40 {
-		if err = calendar.GetRequest(HolidaysURL); err != nil {
-			return
-		} else {
-			if err = calendar.GetPublicHolidays(); err != nil {
-				return
 			}
 		}
-	} else {
-		return fmt.Errorf("year isn't valid, method support year since 2000 to (current + 40)")
 	}
 	return nil
-}*/
-/*calendar.HolidaysCache == nil && */
+}
 
-func (calendar *HolidaysCalendar) GetPublicHolidays(serviceURL string) (*[]GetPublicHolidaysResponse, error) { //publicHolidays...GetPublicHolidaysResponse) error {
-	errorString := "error occurred in holidaysService.GetPublicHolidays(): "
-	publicHolidays := make([]GetPublicHolidaysResponse, AverageNumberOfPublicHolidaysPerYear)
+func (calendar *HolidaysCalendar) IsHolidayToday() (bool, string, error) {
+	if err := calendar.initCaches(); err != nil {
+		return false, "", err
+	}
+
+	today := time.Now()
+	today = time.Date(today.Year(), 6, 20, 0, 0, 0, 0, time.UTC)
+	//today = time.Date(today.Year(), today.Month(), today.Day(), 0, 0, 0, 0, time.UTC)
+	sameTime := false
+	holidayName := ""
+	// compare time with time.Equal()
+	for index, _ := range calendar.HolidaysDatesList {
+		sameTime = today.Equal(calendar.HolidaysDatesList[index])
+		if sameTime {
+			holidayName = calendar.HolidaysNamesList[index]
+			break
+		}
+	}
+	return sameTime, holidayName, nil
+}
+
+func (calendar *HolidaysCalendar) getPublicHolidays(serviceURL string) (*[]GetPublicHolidaysResponse, error) { //publicHolidays...GetPublicHolidaysResponse) error {
+	errorString := "error occurred in holidaysService.getPublicHolidays(): "
+	publicHolidays := make([]GetPublicHolidaysResponse, 0, AverageNumberOfPublicHolidaysPerYear)
 	if url, err := url.Parse(serviceURL); err != nil {
 		errorString += err.Error()
 	} else {
@@ -149,75 +133,41 @@ func (calendar *HolidaysCalendar) GetPublicHolidays(serviceURL string) (*[]GetPu
 }
 
 func (calendar *HolidaysCalendar) GetNearHolidaysInfo() (nearHolidaysInfo string, error error) {
-	todayDate := time.Now()
+	nearHolidaysInfo = fmt.Sprintf("The next holiday is %s, %v %v", calendar.HolidaysNamesList[0], calendar.HolidaysDatesList[0].Month(), calendar.HolidaysDatesList[0].Day())
+	holidayDate := calendar.HolidaysDatesList[0]
+	weekday := calendar.HolidaysDatesList[0].Weekday()
+	longWeekendsString := ", and the weekend will last 3 days: "
+	switch weekday {
+	case 1:
+		firstWeekend := holidayDate.Add(-24 * 2 * time.Hour)
+		temp := fmt.Sprintf("%v %v - %v %v.\n", firstWeekend.Month(), firstWeekend.Day(), holidayDate.Month(), holidayDate.Day())
+		nearHolidaysInfo += longWeekendsString + temp
+	case 5, 6:
+		lastWeekend := holidayDate.Add(24 * 2 * time.Hour)
+		temp := fmt.Sprintf("%v %v - %v %v.\n", holidayDate.Month(), holidayDate.Day(), lastWeekend.Month(), lastWeekend.Day())
+		nearHolidaysInfo += longWeekendsString + temp
+	case 0:
+		firstWeekend := holidayDate.Add(-24 * time.Hour)
+		lastWeekend := holidayDate.Add(24 * time.Hour)
+		temp := fmt.Sprintf("%v %v - %v %v.\n", firstWeekend.Month(), firstWeekend.Day(), lastWeekend.Month(), lastWeekend.Day())
+		nearHolidaysInfo += longWeekendsString + temp
+	}
+	return nearHolidaysInfo, nil
+}
 
-	calendar.HolidaysCash = make(map[float64]time.Time)
-
-	for _, value := range *calendar.PublicHolidaysResponse {
-		if holidayDate, err := time.Parse(CustomTimeFormat, value.DateStringFormat); err != nil {
-			return "", err
+// Method support year since 2000 to current year + 40 (2060). Country code format: "UA", "AT", "BE", "GB" (2 chars)
+/*
+	if year >= 2000 && year <= time.Now().Year()+40 {
+		if err = calendar.GetRequest(HolidaysURL); err != nil {
+			return
 		} else {
-			duration := math.Abs(todayDate.Sub(holidayDate).Hours())
-			calendar.HolidaysCash[duration] = holidayDate
+			if err = calendar.getPublicHolidays(); err != nil {
+				return
+			}
 		}
+	} else {
+		return fmt.Errorf("year isn't valid, method support year since 2000 to (current + 40)")
 	}
-	fmt.Println(len(calendar.HolidaysCash))
-	/*todayStringFormat = today.Format(customDateFormat)
-	fmt.Println(todayStringFormat)*/
-	return
-}
-
-// time zone
-func diff(a, b time.Time) (year, month, day, hour, min, sec int) {
-	if a.Location() != b.Location() {
-		b = b.In(a.Location())
-	}
-	if a.After(b) {
-		a, b = b, a
-	}
-	y1, M1, d1 := a.Date()
-	y2, M2, d2 := b.Date()
-
-	h1, m1, s1 := a.Clock()
-	h2, m2, s2 := b.Clock()
-
-	year = int(y2 - y1)
-	month = int(M2 - M1)
-	day = int(d2 - d1)
-	hour = int(h2 - h1)
-	min = int(m2 - m1)
-	sec = int(s2 - s1)
-
-	// Normalize negative values
-	if sec < 0 {
-		sec += 60
-		min--
-	}
-	if min < 0 {
-		min += 60
-		hour--
-	}
-	if hour < 0 {
-		hour += 24
-		day--
-	}
-	if day < 0 {
-		// days in month:
-		t := time.Date(y1, M1, 32, 0, 0, 0, 0, time.UTC)
-		day += 32 - t.Day()
-		month--
-	}
-	if month < 0 {
-		month += 12
-		year--
-	}
-
-	return
-}
-
-func getMin(values ...float64) float64 {
-	//min := values[0]
-	return 0
-}
-
-//func getCountriesCodesFromString()
+	return nil
+}*/
+/*calendar.HolidaysCache == nil && */
--- main/main.go
+++ main/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	hol "github.com/catdog93/test-task/holidaysService"
 	"log"
 )
@@ -9,15 +10,25 @@ func main() {
 	// input
 	calendar := &hol.HolidaysCalendar{}
 	//calendar.GetHolidaysForThisYear(2019, "UA")*/
-	if PublicHolidaysResponse, err := calendar.GetPublicHolidays(hol.PublicHolidaysURL); err != nil {
+
+	//prints if it’s a holiday today (and the name of it). If today isn’t a holiday, the application should print the next closest holiday.
+
+	if sameTime, holidayName, err := calendar.IsHolidayToday(); err != nil {
 		log.Fatal(err)
 	} else {
-		calendar.PublicHolidaysResponse = PublicHolidaysResponse
-
-		if _, err := calendar.GetNearHolidaysInfo(); err != nil {
-			log.Fatal(err)
+		if sameTime {
+			fmt.Printf("Holiday: %s is today !", holidayName)
+		} else {
+			if info, err := calendar.GetNearHolidaysInfo(); err != nil {
+				log.Fatal(err)
+			} else {
+				fmt.Println(info)
+				/*	fmt.Println(calendar.HolidaysNamesList)
+					fmt.Println(calendar.HolidaysDatesList)
+					fmt.Println(calendar.HolidaysDifferenceSinceNowList)*/
+			}
 		}
-
-		//fmt.Println(calendar.PublicHolidaysResponse)
 	}
+
+	//The next holiday is International Workers' Day, May 1, and the weekend will last 3 days: May 1 - May 3
 }
